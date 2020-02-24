47f8e5e19487db51244c10ed9e4adbda3a9012e4
marcinreszko <48009949+marcinreszko@users.noreply.github.com>
2020-02-24 15:31:25 +0100

    Added wrapper div and changed overlay into modal's sibling

--- src/shared/modal/Modal.tsx
+++ src/shared/modal/Modal.tsx
@@ -8,14 +8,20 @@ import { useEscapeAction } from './useEscapeAction';
 import Portal from './Portal';
 
 const cssTransitionTime = 300;
+
 const overlayClosed = [classes.overlay, classes.overlayClosed].join(' ');
 const overlayWillOpen = [classes.overlay, classes.overlayWillOpen].join(' ');
 
+const modalClosed = [classes.modalContainer, classes.overlayClosed].join(' ');
+const modalWillOpen = [classes.modalContainer, classes.overlayWillOpen].join(' ');
+
 const Modal = ({ handleClose, children, open, size }: ModalProps) => {
   const [overlayClasses, setOverlayClasses] = useState(overlayClosed);
+  const [modalClasses, setModalClasses] = useState(modalClosed);
 
   const closeModal = () => {
     setOverlayClasses(overlayClosed);
+    setModalClasses(modalClosed);
     setTimeout(() => handleClose(), cssTransitionTime);
   };
 
@@ -23,9 +29,13 @@ const Modal = ({ handleClose, children, open, size }: ModalProps) => {
 
   useEffect(() => {
     if (open) {
-      setTimeout(() => setOverlayClasses(overlayWillOpen), 10);
+      setTimeout(() => {
+        setOverlayClasses(overlayWillOpen);
+        setModalClasses(modalWillOpen);
+      }, 10);
     } else {
       setOverlayClasses(overlayClosed);
+      setModalClasses(modalClosed);
     }
   }, [open]);
 
@@ -35,14 +45,14 @@ const Modal = ({ handleClose, children, open, size }: ModalProps) => {
 
   return (
     <Portal id="modal">
-      <div className={overlayClasses}>
-        <div className={classes.modalContainer}>
+      <div className={classes.wrapper}>
+        <div className={overlayClasses}></div>
+        <div className={modalClasses}>
           <div className={classes.modalHeader}>
             <button onClick={closeModal} className={classes.buttonClose}>
               X
             </button>
           </div>
-
           <div className={contentClasses}>{children}</div>
         </div>
       </div>
