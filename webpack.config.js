afd56eb03a569be752813d01bf4ca56663df0fd7
polubis <polubis@wp.pl>
2020-02-27 14:37:01 +0100

    Improve webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,41 @@ const path = require('path');
 const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const InterpolateHtmlPlugin = require('interpolate-html-plugin');
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = (env, { mode }) => {
+  const [PROD] = ['production'];
   console.log(`App is running in ${mode} mode`);
 
+  const optimization =
+    mode === PROD
+      ? {
+          runtimeChunk: 'single',
+          splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+              default: false,
+              
+              vendor: {
+                test: /[\\/]node_modules[\\/]/,
+                name(module) {
+                  // We creating here node_modules single package name
+                  // We replacing @ with '' because some servers don't like @ - for ex. in @babel
+                  return `npm.${module.context
+                    .match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                    .replace('@', '')}`;
+                }
+              }
+            }
+          }
+        }
+      : {};
+
   return {
-    devtool: 'eval-source-map',
-    entry: './src/index.tsx',
+    devtool: mode !== PROD && 'eval-source-map',
+    entry: path.resolve(__dirname, 'src/index.tsx'),
     resolve: {
       extensions: ['.ts', '.tsx', '.js', 'json'],
       alias: {
@@ -21,9 +47,10 @@ module.exports = (env, { mode }) => {
     },
     output: {
       path: __dirname + '/dist',
-      filename: 'bundle.min.js',
+      filename: '[name].[hash].js',
       publicPath: '/'
     },
+    optimization,
     module: {
       rules: [
         {
@@ -85,6 +112,9 @@ module.exports = (env, { mode }) => {
       }),
       new InterpolateHtmlPlugin({
         PUBLIC_URL: 'public'
+      }),
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false
       })
     ],
     devServer: {
