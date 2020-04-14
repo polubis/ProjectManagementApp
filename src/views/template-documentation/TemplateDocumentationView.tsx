f8d9ac5ce91c2ae7415a223e332b8e787ab822d0
marcinreszko <48009949+marcinreszko@users.noreply.github.com>
2020-04-14 12:58:09 +0200

    Changed variable names
    Deleted not needed variables
    Fixed re-render issue

--- src/views/template-documentation/TemplateDocumentationView.tsx
+++ src/views/template-documentation/TemplateDocumentationView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import InfoIcon from '@material-ui/icons/Info';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
@@ -6,10 +6,16 @@ import ExploreIcon from '@material-ui/icons/Explore';
 import WarningIcon from '@material-ui/icons/Warning';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+import { Button } from 'shared/ui';
 
 import { DocumentationSection } from './models';
 
 import csx from './TemplateDocumentationView.scss';
+
+
 const SECTIONS_MOCKED: DocumentationSection[] = [
   {
     title: 'Basic informations',
@@ -36,59 +42,51 @@ const SECTIONS_MOCKED: DocumentationSection[] = [
 ];
 
 const TemplateDocumentationView = () => {
-
-  const [activeSection, setActiveSection] = useState(SECTIONS_MOCKED[0].title);
+  const [activeSection, setActiveSection] = useState(0);
   const [sections, setSections] = useState(SECTIONS_MOCKED);
-  const [sectionsModified, setSectionsModified] = useState(false);
 
   const addNewSection = () => {
     const section: DocumentationSection = {
       icon: <ExploreIcon />,
-      title: 'essa'
+      title: 'siema'
     };
 
-    const combinedSections = [
-      ...sections,
-      section
-    ]
+    const combinedSections = [...sections, section];
 
     setSections(combinedSections);
-    setSectionsModified(!sectionsModified);
   };
 
   const addNewSubSection = (sectionIndex: number) => {
-    const combinedSections = sections;
-    const dummySection = sections[sectionIndex];
+    const combinedSections = [...sections];
+    const newSubSections = {...sections[sectionIndex]};
 
-    dummySection.subSection.push('subSection');
+    newSubSections.subSection.push('subSection');
 
-    combinedSections[sectionIndex] = dummySection;
+    combinedSections[sectionIndex] = newSubSections;
 
     setSections(combinedSections);
-    setSectionsModified(!sectionsModified);
-  }
-
-  useEffect(() => {
-  }, [sectionsModified]);
+  };
 
   const mapSection = (section: DocumentationSection[]) => {
-    const mappedSection = section.map((value, index) => {
+    const mappedSection = section.map((value, idx) => {
       if (value.subSection)
         return (
-          <li className={csx.listElement} key={value.title}>
+          <li className={csx.listElement} key={value.title} data-idx={idx}>
             <span
-              className={activeSection === value.title ? csx.active : null}
-              onClick={() => setActiveSection(value.title)}
+              className={`${activeSection === idx ? csx.active : ''}`}
+              onClick={() => setActiveSection(idx)}
             >
               {value.icon}
               {value.title}
             </span>
+
             <ul className={csx.nestedList}>
               {value.subSection.map((value) => (
                 <li>{value}</li>
               ))}
             </ul>
-            <span className={csx.listElement} onClick={() => addNewSubSection(index)}>
+
+            <span className={csx.listElement} onClick={() => addNewSubSection(idx)}>
               <span>
                 <AddCircleOutlineIcon /> ADD SUBSECTION
               </span>
@@ -97,10 +95,10 @@ const TemplateDocumentationView = () => {
         );
 
       return (
-        <li className={csx.listElement} key={value.title}>
+        <li className={csx.listElement} key={value.title} data-idx={idx}>
           <span
-            className={activeSection === value.title ? csx.active : null}
-            onClick={() => setActiveSection(value.title)}
+            className={`${activeSection === idx ? csx.active : ''}`}
+            onClick={() => setActiveSection(idx)}
           >
             {value.icon}
             {value.title}
@@ -123,6 +121,10 @@ const TemplateDocumentationView = () => {
   return (
     <div className={csx.sectionContainer}>
       <ul className={csx.sectionList}>{mapSection(sections)}</ul>
+      <Button variant="icon" className={csx.button}>
+        <ChevronLeftIcon />
+        <ChevronRightIcon />
+      </Button>
     </div>
   );
 };
