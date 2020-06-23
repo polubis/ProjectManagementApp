38751c3edbdbc47fc6c642743f4fdcf2cf28b15d
polubis <polubis@wp.pl>
2020-06-23 18:23:08 +0100

    Create modules folder

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,10 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Modules from 'modules';
 
-import { Auth } from 'core/auth';
-import TechnologiesProvider from 'core/technologies';
-
 const App = () => {
   return (
     <BrowserRouter>
-      <Auth.Provider>
-        <TechnologiesProvider>
-          <Modules />
-        </TechnologiesProvider>
-      </Auth.Provider>
+      <Modules />
     </BrowserRouter>
   );
 };
--- src/modules/Modules.tsx
+++ src/modules/Modules.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import { Auth } from 'core/auth';
+import TechnologiesProvider from 'core/technologies';
+
 import { ModulesRouter } from '.';
 
 const Modules = () => {
-  return <ModulesRouter />;
+  return (
+    <Auth.Provider>
+      <TechnologiesProvider>
+        <ModulesRouter />
+      </TechnologiesProvider>
+    </Auth.Provider>
+  );
 };
 
 export default Modules;
