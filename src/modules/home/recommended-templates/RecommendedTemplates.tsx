d7864d1f3ed4793392e33d5f5cc4ffb30693a39b
iuklaw <plaziuk@gmail.com>
2020-06-25 22:09:06 +0200

    home reccomended templates links

--- src/modules/home/recommended-templates/RecommendedTemplates.tsx
+++ src/modules/home/recommended-templates/RecommendedTemplates.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
+import { useHistory } from 'react-router';
 
 import { TemplateTile } from 'src/modules/templates';
 
@@ -7,15 +9,25 @@ import { templatesMock } from 'src/mocks/templates';
 import csx from './RecommendedTemplates.scss';
 
 export const RecommendedTemplates = () => {
+  const { push, location } = useHistory();
+
+  const goToDetails = useCallback(
+    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      const templateId = e.currentTarget.getAttribute('data-id');
+      push(`app/templates/all/${templateId}`);
+    },
+    [location]
+  );
+
+  const recommendedTemplates = templatesMock.slice(0, 4);
+  
   return (
     <section className={csx.recommendedTemplates}>
       <div className={csx.templatesContainer}>
         <h2 className={csx.heading}>Recommended templates</h2>
-
-        <TemplateTile onClick={() => console.log('siema')} template={templatesMock[0]} />
-        <TemplateTile onClick={() => console.log('siema')} template={templatesMock[1]} />
-        <TemplateTile onClick={() => console.log('siema')} template={templatesMock[2]} />
-        <TemplateTile onClick={() => console.log('siema')} template={templatesMock[3]} />
+        {recommendedTemplates.map((template, idx) => {
+          return <TemplateTile onClick={goToDetails} template={template} key={idx} />;
+        })}
       </div>
     </section>
   );
