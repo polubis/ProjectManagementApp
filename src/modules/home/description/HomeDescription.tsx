e69016d67878a23a2f790cc4da91439b02a6154c
iuklaw <plaziuk@gmail.com>
2020-06-25 22:59:53 +0200

    fixed home description link

--- src/modules/home/description/HomeDescription.tsx
+++ src/modules/home/description/HomeDescription.tsx
@@ -16,13 +16,11 @@ export const HomeDescription = () => {
       <div className={csx.textSection}>
         <p className={csx.header}>The whole <span>ecosystem</span> in one.</p>
         <p className={csx.desc}>Choose a template, find people in the team, create a project and enjoy the whole ecosystem in one place.</p>
-        <Guard.Unprotected>
-        <NavLink to="/register">
+        <NavLink to="/app/templates">
         <Button>
           START JOURNEY
         </Button>
         </NavLink>
-        </Guard.Unprotected>
       </div>
       <div className={csx.imageSection}>
         <EcoSystemImg />
