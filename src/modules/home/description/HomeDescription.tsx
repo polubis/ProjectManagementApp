de3be11988c92a258440316b68b17c3623848e6b
iuklaw <plaziuk@gmail.com>
2020-06-25 22:08:32 +0200

    home description link

--- src/modules/home/description/HomeDescription.tsx
+++ src/modules/home/description/HomeDescription.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { NavLink } from 'react-router-dom';
+
+import { Guard } from 'core/auth';
+
 import { Button } from 'ui';
 
 import { EcoSystemImg } from './EcoSystemImg';
@@ -12,9 +16,13 @@ export const HomeDescription = () => {
       <div className={csx.textSection}>
         <p className={csx.header}>The whole <span>ecosystem</span> in one.</p>
         <p className={csx.desc}>Choose a template, find people in the team, create a project and enjoy the whole ecosystem in one place.</p>
+        <Guard.Unprotected>
+        <NavLink to="/register">
         <Button>
           START JOURNEY
         </Button>
+        </NavLink>
+        </Guard.Unprotected>
       </div>
       <div className={csx.imageSection}>
         <EcoSystemImg />
