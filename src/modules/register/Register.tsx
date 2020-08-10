8d8278cfc4a36d03737e4ad02a71a01e9a236fe9
Domin <teesdomin@gmail.com>
2020-08-10 23:01:31 +0200

    Add Navbar and Footer components to Register

--- src/modules/register/Register.tsx
+++ src/modules/register/Register.tsx
@@ -4,12 +4,21 @@ import { useTechnologiesProvider } from 'core/technologies';
 
 import RegisterForm from './register-form';
 
+import Navbar from "shared/components/navbar";
+import Footer from "shared/components/footer";
+
 import csx from './Register.scss';
 
 const Register = () => {
   const { loading: loadingTechnologies } = useTechnologiesProvider();
 
-  return <div className={csx.register}>{!loadingTechnologies && <RegisterForm />}</div>;
+  return (
+    <>
+      <Navbar/>
+      <div className={csx.register}>{!loadingTechnologies && <RegisterForm/>}</div>
+      <Footer/>
+    </>
+  )
 };
 
 export default Register;
