3bff3147f5e64f2ea961568b0f0629bc0cce20ea
polubis <polubis@wp.pl>
2020-08-14 16:02:42 +0200

    Add bars to content tree

--- src/modules/template-documentation/content-tree/tree-item/TreeItem.tsx
+++ src/modules/template-documentation/content-tree/tree-item/TreeItem.tsx
@@ -21,14 +21,15 @@ namespace TreeItem {
 }
 
 const ITEM_INDENDATION = 20,
-  ULTER_ITEM_INDENDATION = 8;
+  ULTER_ITEM_INDENDATION = 8,
+  DOT_ADDITIONAL_SPACE = 1;
 
 const TreeItem = ({
   style,
   index,
   data: { activeItem, expandedItems, items, onClick }
 }: TreeItem.Props) => {
-  const { id, childrenCount, label, level } = items[index];
+  const { id, childrenCount, label, level, parentId } = items[index];
 
   return (
     <div
@@ -38,14 +39,27 @@ const TreeItem = ({
       data-idx={id}
       style={{
         ...style,
-        paddingLeft: `${
-          level * ITEM_INDENDATION + (level === 0 ? 0 : level * ULTER_ITEM_INDENDATION)
-        }px`
+        paddingLeft: `${level * ITEM_INDENDATION +
+          (level === 0 ? 0 : level * ULTER_ITEM_INDENDATION)}px`
       }}
       onClick={onClick}
     >
+      {expandedItems[parentId] && (
+        <div
+          className={csx.dot}
+          style={{
+            left: `${level * ITEM_INDENDATION -
+              ITEM_INDENDATION +
+              (level === 1 ? 0 : ULTER_ITEM_INDENDATION + DOT_ADDITIONAL_SPACE)}px`
+          }}
+        />
+      )}
+      
       {childrenCount > 0 && <ChevronIcon />}
-      <span title={label}>{label}</span>
+      
+      <span className={csx.label} title={label}>
+        {label}
+      </span>
     </div>
   );
 };
