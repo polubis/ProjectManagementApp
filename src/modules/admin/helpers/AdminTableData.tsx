4b07965a76e91855501e60af58c52eb48dd25ecd
marious9 <mariuszK9@hotmail.com>
2020-08-26 16:15:30 +0200

    Added fixed size list to admin tabs

--- src/modules/admin/helpers/AdminTableData.tsx
+++ src/modules/admin/helpers/AdminTableData.tsx
@@ -9,44 +9,80 @@ const avatarUrl = 'https://cdn.pixabay.com/photo/2018/04/28/13/18/man-3357275_96
 
 export const header = ['', 'name', 'description', 'created', 'modified', 'added by', ''];
 
-const formatValue = (value: string, cutIndex: number) : string =>
-  value.length > cutIndex
-    ? value.substr(0, cutIndex - 3) + '...'
-    : value;
+const WIDTHS = {
+  id: 30,
+  name: 226,
+  description: 347,
+  created: 110,
+  modified: 110,
+  addedBy: 90,
+  icon: 80
+};
 
-export const getTableData = (data) => { // @TODO Add the interface when backend will provide full model
-    const tableData : Table.Row[] = data.map((item) => {
-      const row: Table.Row = {
-        id: {
-          component: <p className={csx.id}>{item.id}</p>
-        },
-        name: {
-          component: <p title={item.name} className={csx.name}>{formatValue(item.name, 31)}</p>
+const formatValue = (value: string, cutIndex: number): string =>
+  value.length > cutIndex ? value.substr(0, cutIndex - 3) + '...' : value;
 
-        },
-        description: {
-        component: <p title={item.description} className={csx.description}>{formatValue(item.description, 94)}</p>
-        },
-        created: {
-          component: '19 / Apr / 2020'
-        },
-        modified: {
-          component: '19 / Apr / 2020'
-        },
-        addedBy: {
-          component:
-            <div className={csx.avatarContainer}>
-              <Avatar className={csx.avatar} src={avatarUrl} />
-              <p>by przemo</p>
-            </div>
-        },
-        icon: {
-          component: <MoreHorizIcon />
+export const getTableData = (data) => {
+  // @TODO Add the interface when backend will provide full model
+  const tableData: Table.Row[] = data.map((item) => {
+    const row: Table.Row = {
+      id: {
+        width: WIDTHS.id,
+        component: <p className={csx.id}>{item.id}</p>
+      },
+      name: {
+        width: WIDTHS.name,
+        component: (
+          <p title={item.name} className={csx.name}>
+            {formatValue(item.name, 31)}
+          </p>
+        )
+      },
+      description: {
+        width: WIDTHS.description,
+        component: (
+          <p title={item.description} className={csx.description}>
+            {formatValue(item.description, 94)}
+          </p>
+        )
+      },
+      created: {
+        width: WIDTHS.created,
+        component: '19 / Apr / 2020'
+      },
+      modified: {
+        width: WIDTHS.modified,
+        component: '19 / Apr / 2020'
+      },
+      addedBy: {
+        width: WIDTHS.addedBy,
+        component: (
+          <div className={csx.avatarContainer}>
+            <Avatar className={csx.avatar} src={avatarUrl} />
+            <p>by przemo</p>
+          </div>
+        )
+      },
+      icon: {
+        width: WIDTHS.created,
+        component: <MoreHorizIcon />
+      }
+    };
+
+    return row;
+  });
+
+  return [
+    header.reduce(
+      (prev, x, i) => ({
+        ...prev,
+        [i]: {
+          component: x,
+          width: Object.values(WIDTHS)[i]
         }
-      };
-  
-      return row;
-    });
-  
-    return tableData;
-  };
+      }),
+      {}
+    ),
+    ...tableData
+  ];
+};
--- src/shared/components/table-grid/Table.tsx
+++ src/shared/components/table-grid/Table.tsx
@@ -1,9 +1,11 @@
 import React, { useMemo } from 'react';
 
 import csx from './Table.scss';
+import { FixedSizeList } from 'react-window';
 
 namespace Table {
   export interface Cell {
+    width?: number;
     component: JSX.Element | string | number;
   }
 
@@ -18,27 +20,50 @@ namespace Table {
 }
 
 const Table = ({ data, header }: Table.Props) => {
-  const renderTableBody = () =>
-    useMemo(
-      () =>
-        data.map((entry) =>
-          Object.values(entry).map((cell, i) => <span key={i}>{cell.component}</span>)
-        ),
-      [data, header]
-    );
-  return (
-    <div
-      style={{
-        gridTemplateColumns: `repeat(${header.length}, 1fr)`
-      }}
-      className={csx.grid}
-    >
-      {header.map((key, i) => (
-        <span key={i} className={csx.header}>
-          {key}
+  const renderTableBody = useMemo(
+    () =>
+      data.map((entry) =>
+        Object.values(entry).map((cell, i) => <span key={i}>{cell.component}</span>)
+      ),
+    [data, header]
+  );
+
+  const headerToData = header.reduce(
+    (prev: Object, x: string, i: number) => ({ ...prev, [i]: { component: x } }),
+    {}
+  );
+
+  const Row = ({ index, style }) => {
+    let currentWidth = 70;
+    const row = Object.values(data[index]).map((cell, i) => {
+      const renderCell = (
+        <span
+          style={{ ...style, left: currentWidth, top: index * style.height, width: '' }}
+          key={i + index}
+        >
+          {cell.component}
         </span>
-      ))}
-      {renderTableBody()}
+      );
+
+      currentWidth += cell.width + 40;
+
+      return renderCell;
+    });
+    return <>{row}</>;
+  };
+
+  return (
+    <div>
+      <FixedSizeList
+        className={csx.grid}
+        height={500}
+        width={1350}
+        itemData={data}
+        itemCount={data.length}
+        itemSize={80}
+      >
+        {Row}
+      </FixedSizeList>
     </div>
   );
 };
