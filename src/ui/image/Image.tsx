21748f59b200d8a7195095014d648e94da77ec5d
AjronN <34678770+ajronn@users.noreply.github.com>
2020-10-07 14:38:51 +0200

    Add fallback, error state, and conditional rendering

--- src/ui/image/Image.tsx
+++ src/ui/image/Image.tsx
@@ -1,42 +1,57 @@
 import React, { useState, useEffect, useRef } from "react"
 
-import { Loader } from "ui"
+import { Loader, Button } from "ui"
 
 import csx from "./Image.scss"
 
 namespace Image {
     export interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {
         className?: string;
-        lowQuality: any;
+        lowQuality: string;
+        fallback?: string;
     }
 }
 
-const Image = ({ lowQuality, src, className, ...props }: Image.Props) => {
-
-    const [url, setUrl] = useState<any>(lowQuality || src)
+const Image = ({ lowQuality, fallback, src, className, ...props }: Image.Props) => {
+    const [url, setUrl] = useState(lowQuality || src)
+    const [error, setError] = useState(false)
 
     useEffect(() => {
-        if (url === lowQuality) {
-            fetch(src).then(e => e.blob()).then(b => {
-                const FR = new FileReader();
-                FR.onload = function () {
-                    setUrl(this.result)
-                };
-                FR.readAsDataURL(b);
-            })
-        }
+        if (url === lowQuality) loadImage();
     }, [])
 
+    const loadImage = () => {
+        setError(false)
+        fetch(src).then(e => e.blob()).then(b => {
+            const FR = new FileReader();
+            FR.onload = function () {
+                setUrl(this.result + "");
+            };
+            FR.readAsDataURL(b);
+        })
+    }
+
+    const onErrorHandler = () => {
+        setError(true);
+        if (fallback) setUrl(fallback)
+    }
+
     return (
-        <div className={`${csx.img}`}
+        <div className={`${csx.img} ${className}`}
             style={{ height: props.height, width: props.width }}>
-            {url === lowQuality && <Loader className={csx.loader} />}
+            {((url === lowQuality) && (error === false))
+                ? <Loader className={`${csx.mask}`} />
+                : ""}
+            {error === true
+                ? <Button className={`${csx.button} ${csx.mask}`} onClick={() => loadImage()}>Retry</Button>
+                : ""}
             <img
-                className={`${className}`}
                 alt={props.alt || "image"}
                 {...{ src: url, ...props }}
                 height={props.height}
-                width={props.width} />
+                width={props.width}
+                onError={onErrorHandler}
+            />
         </div>
     )
 }
