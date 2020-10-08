e05eb2a847c7ce2df6a69477b71ba6f35b2ae6a2
AjronN <34678770+ajronn@users.noreply.github.com>
2020-10-08 10:10:45 +0200

    fix file

--- src/modules/home/sections/search/Search.tsx
+++ src/modules/home/sections/search/Search.tsx
@@ -6,26 +6,23 @@ import { TemplatesSearch } from 'shared/components';
 
 import csx from './Search.scss';
 
-import { Image } from "ui";
-
 const Search = () => {
   return (
-    <div>
-      <section className={csx.search}>
-        <h1>
-          <span>Simplified</span> and faster project development process
-          </h1>
+    <section className={csx.search}>
+      <h1>
+        <span>Simplified</span> and faster project development process
+      </h1>
 
-        <p>
-          Use <span>Jupi.io</span> to boost your development process with high quality templates
+      <p>
+        Use <span>Jupi.io</span> to boost your development process with high quality templates
         prepared by developers community from around the world
       </p>
 
-        <TemplatesSearch
-          className={csx.templatesSearch}
-          pathname={`/app/templates/${TemplateCategory.ALL}`}
-        />
-      </section></div>
+      <TemplatesSearch
+        className={csx.templatesSearch}
+        pathname={`/app/templates/${TemplateCategory.ALL}`}
+      />
+    </section>
   );
 };
 
