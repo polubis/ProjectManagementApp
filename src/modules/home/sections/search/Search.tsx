9c31ab48a8e95b8885d9d2723bd415be35ab2e9d
AjronN <34678770+ajronn@users.noreply.github.com>
2020-10-08 10:03:44 +0200

    fix

--- src/modules/home/sections/search/Search.tsx
+++ src/modules/home/sections/search/Search.tsx
@@ -11,20 +11,10 @@ import { Image } from "ui";
 const Search = () => {
   return (
     <div>
-      <Image src="https://images.unsplash.com/photo-1427847888090-7b66d035cdaa?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1800&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=3200"
-        lowQuality="https://images.unsplash.com/photo-1427847888090-7b66d035cdaa?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=9&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=16"
-        height={500}
-        width={500} />
-      <Image src="https://images.unsplash.com/photo-1427847888090-7b66d035cdaa?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1800&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=3200"
-        lowQuality="https://images.unsplash.com/photo-1427847888090-7b66d035cdaa?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=9&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=16"
-        height={500}
-        width={500} />
       <section className={csx.search}>
-
-
         <h1>
           <span>Simplified</span> and faster project development process
-      </h1>
+          </h1>
 
         <p>
           Use <span>Jupi.io</span> to boost your development process with high quality templates
