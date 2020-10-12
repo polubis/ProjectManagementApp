71b36557725209832d40d9094e11fc8c06263b79
Czaroos <czaroosik@gmail.com>
2020-10-12 14:23:15 +0200

    Added title and meta description tags for Home, Login and Register sections. Also added alt attribute for images.

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import { Footer, Navbar } from 'shared/components';
 
@@ -7,6 +8,13 @@ import { Ecosystem, Features, Search, RecentTemplates } from './sections';
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>Jupi.io - Simplified and faster project development process </title>
+        <meta
+          name="description"
+          content="Use Jupi.io to boost your development process with high quality templates prepared by developers community from around the world!"
+        />
+      </Helmet>
       <Navbar />
       <Search />
       <Features />
--- src/ui/logo/Logo.tsx
+++ src/ui/logo/Logo.tsx
@@ -5,5 +5,5 @@ import { IMGS_PATH } from 'consts';
 import csx from './Logo.scss';
 
 export const Logo = () => {
-  return <img className={csx.root} src={IMGS_PATH + '/Logo.png'} />;
+  return <img className={csx.root} src={IMGS_PATH + '/Logo.png'} alt="Jupi.io Logo" />;
 };
