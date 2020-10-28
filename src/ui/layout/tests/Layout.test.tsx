a46993d3dec13931bb6cc86b13b0e9307fa287c4
AjronN <34678770+ajronn@users.noreply.github.com>
2020-10-28 12:40:03 +0100

    add tests

--- src/ui/layout/tests/Layout.test.tsx
+++ src/ui/layout/tests/Layout.test.tsx
@@ -3,3 +3,30 @@ import { render, screen, fireEvent } from '@testing-library/react';
 
 import { Layout } from "..";
 
+
+describe('<Layout>', () => {
+    it("shows children", () => {
+        const child = <div data-testid="child">Test</div>;
+        render(<Layout>{child}</Layout>)
+        expect(screen.getByTestId("child")).toBeDefined();
+    })
+
+    it("style pass correctly", () => {
+        const child = <div data-testid="child">Test</div>;
+        const { container } = render(<Layout className={"foo"}>{child}</Layout>)
+        expect(container.firstChild).toHaveClass('foo');
+    })
+
+    it("scroll var change height property", () => {
+        const child = <div data-testid="child">Test</div>;
+        render(<Layout scroll={true}>{child}</Layout>)
+        const props = {
+            scroll: true,
+            children: child
+        }
+        const layoutClass = Layout(props).type.styledComponentId;
+        const layoutRoots = document.getElementsByClassName(layoutClass);
+        const style = window.getComputedStyle(layoutRoots[0]);
+        expect(style.height).toBe('110%');
+    })
+});
