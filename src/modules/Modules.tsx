6b379854d6ced0e0f88be97b2d4a5ebbea5203be
Domin <teesdomin@gmail.com>
2020-10-28 19:52:08 +0100

    Wrap modules with CookieProvider

--- src/modules/Modules.tsx
+++ src/modules/Modules.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { AlertsManager } from 'core/api';
 import AuthProvider from 'core/auth';
+import CookiesProvider from 'core/cookies';
 import PatternsProvider from 'core/patterns';
 import TechnologiesProvider from 'core/technologies';
 
@@ -11,13 +12,15 @@ const Modules = () => {
   return (
     <>
       <AlertsManager />
-      <AuthProvider>
-        <PatternsProvider>
-          <TechnologiesProvider>
-            <ModulesRouter />
-          </TechnologiesProvider>
-        </PatternsProvider>
-      </AuthProvider>
+      <CookiesProvider>
+        <AuthProvider>
+          <PatternsProvider>
+            <TechnologiesProvider>
+              <ModulesRouter />
+            </TechnologiesProvider>
+          </PatternsProvider>
+        </AuthProvider>
+      </CookiesProvider>
     </>
   );
 };
