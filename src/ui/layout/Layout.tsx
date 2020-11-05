9f9f6e4be308dc717a76d7d41e856e69f4fee6b2
polubis <polubis@wp.pl>
2020-11-05 18:26:01 +0200

    Improve code

--- src/ui/layout/Layout.tsx
+++ src/ui/layout/Layout.tsx
@@ -1,22 +1,19 @@
-import React, { ReactNode } from "react"
+import React, { ReactNode } from 'react';
 
-import csx from "./Layout.scss"
+import csx from './Layout.scss';
 
 namespace Layout {
-    export interface Props {
-        className?: string;
-        children: ReactNode;
-        overgrown?: boolean;
-    }
+  export interface Props {
+    className?: string;
+    children: ReactNode;
+    overgrown?: boolean;
+  }
 }
 
 const Layout = ({ className, children, overgrown = false }: Layout.Props) => {
-    return (
-        <div className={`${csx.layout} ${className}`}
-            style={{ height: overgrown ? "110%" : "100%" }}>
-            {children}
-        </div>
-    )
-}
+  return (
+    <div className={`${csx.layout} ${className} ${overgrown ? csx.overgrown : ''}`}>{children}</div>
+  );
+};
 
 export default Layout;
--- src/ui/layout/tests/Layout.test.tsx
+++ src/ui/layout/tests/Layout.test.tsx
@@ -1,29 +1,24 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
-import { Layout } from "..";
-
+import { Layout } from '..';
 
 describe('<Layout>', () => {
-    it("shows children", () => {
-        render(<Layout><div>Test</div></Layout>)
-        expect(screen.getByText("Test")).toBeInTheDocument();
-    })
+  it('shows children', () => {
+    render(
+      <Layout>
+        <div>Test</div>
+      </Layout>
+    );
+    expect(screen.getByText('Test')).toBeInTheDocument();
+  });
 
-    it("style pass correctly", () => {
-        const { container } = render(<Layout className={"foo"}><div>Test</div></Layout>)
-        expect(container.firstChild).toHaveClass('foo');
-    })
-
-    it("displays browser scroll for overgrown property", () => {
-        render(<Layout overgrown={true}><div>Test</div></Layout>)
-        const props = {
-            scroll: true,
-            children: <div>Test</div>
-        }
-        const layoutClass = Layout(props).type.styledComponentId;
-        const layoutRoots = document.getElementsByClassName(layoutClass);
-        const style = window.getComputedStyle(layoutRoots[0]);
-        expect(style.height).toBe('110%');
-    })
+  it('adds className', () => {
+    const { container } = render(
+      <Layout className="foo">
+        <div>Test</div>
+      </Layout>
+    );
+    expect(container.firstChild).toHaveClass('foo');
+  });
 });
