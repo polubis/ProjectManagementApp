e7bf97f20e7eb2926f3dcd5c85994d88a81fedf9
polubis <polubis@wp.pl>
2020-12-09 14:58:04 +0100

    Add modules loader component

--- src/modules/base/components/index.ts
+++ src/modules/base/components/index.ts
@@ -1,4 +1,3 @@
 export * from './footer';
 export * from './mobile-navigation';
 export * from './navbar';
-export * from './page-loader';
--- src/modules/base/components/page-loader/PageLoader.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import csx from './PageLoader.scss';
-
-const PageLoader = (): JSX.Element => <div className={csx.pageLoader} />;
-
-export default PageLoader;
