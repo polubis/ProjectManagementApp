deec91c54eade463f888fb69d3c6e132a3b77354
polubis <polubis@wp.pl>
2020-12-18 14:17:15 +0100

    Add basic alerts usage

--- src/utils/api/createInstance.ts
+++ src/utils/api/createInstance.ts
@@ -1,62 +1,5 @@
 import Axios, { AxiosError, AxiosResponse, AxiosRequestConfig, AxiosInstance } from 'axios';
 
-// interface Instance extends AxiosInstance {
-//   get: <R = any>(url: string, config?: AxiosRequestConfig) => Promise<R>;
-//   delete: <R = any>(url: string, config?: AxiosRequestConfig) => Promise<R>;
-//   post: <R = any>(url: string, data?: any, config?: AxiosRequestConfig) => Promise<R>;
-//   put: <R = any>(url: string, data?: any, config?: AxiosRequestConfig) => Promise<R>;
-//   patch: <R = any>(url: string, data?: any, config?: AxiosRequestConfig) => Promise<R>;
-//   subscribe: (subscriber: Subscriber) => void;
-//   unsubscribe: () => void;
-// }
-
-// // createInstance<Model>(config)
-
-// export const createInstance = (config: AxiosRequestConfig) => (
-//   parseSuccess: Success,
-//   parseError: Error
-// ) => (errorsBlackList: string[]): Instance => {
-//   let subscriber: Subscriber = null;
-
-//   const notify = (err: any) => {
-//     subscriber(err);
-//   };
-
-//   const cutUrl = (url: string) => {
-//     const queryIdx = url.indexOf('?');
-//     return queryIdx > -1 ? url.slice(0, queryIdx) : url;
-//   };
-
-//   const handleParseError = (err: AxiosError) => {
-//     const parsed = parseError(err);
-
-//     const url = cutUrl(err.response.config.url);
-
-//     if (!errorsBlackList.includes(url)) {
-//       notify(parsed);
-//     }
-
-//     return Promise.reject(parsed);
-//   };
-
-//   const subscribe = (newSubscriber: Subscriber) => {
-//     subscriber = newSubscriber;
-//   };
-
-//   const unsubscribe = () => {
-//     subscriber = null;
-//   };
-
-//   const instance = Axios.create(config) as Instance;
-
-//   instance.interceptors.response.use(
-//     (res) => parseSuccess(res),
-//     (err) => handleParseError(err)
-//   );
-
-//   return { ...instance, subscribe, unsubscribe } as Instance;
-// };
-
 interface Config<T, ER> extends AxiosRequestConfig {
   parseSuccess?(res: AxiosResponse<T>): any;
   parseError?(res: AxiosError<T>): ER;
@@ -87,27 +30,6 @@ export const createInstance = <T extends Record<keyof T, T[keyof T]>, ER>({
   };
 
   instance.interceptors.response.use(onSuccess, onError);
-
-  // let successParser = (res) => res;
-  // let errorParser = (res) => res;
-
-  // instance.addSuccessParser = (parser: SuccessParser<T, unknown>): void => {
-  //   successParser = parser;
-  // };
-
-  // instance.addErrorParser = (parser: ErrorParser<T, unknown>): void => {
-  //   errorParser = parser;
-  // };
-
-  // instance.interceptors.response.use(
-  //   (res) => successParser(res),
-  //   (err) => errorParser(err)
-  // );
-
-  // instance.addErrorParser = (parser: SuccessParser<R>): void => {
-  //   successParser = parser;
-  // };
-  // instance.addErrorParser();
 
   return instance;
 };
